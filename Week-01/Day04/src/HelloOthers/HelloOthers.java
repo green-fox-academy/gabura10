a223dc9b522f0a3773202b7e4daff373a77ea3c1
gabura10 <gabura.alex10@gmail.com>
2020-05-21 14:44:57 +0200

    Completed the first tasks

---
+++ Week-01/Day04/src/HelloOthers/HelloOthers.java
@@ -0,0 +1,9 @@
+package HelloOthers;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Móni!");
+        System.out.println("Hello Eszter!");
+        System.out.println("Hello Viki!");
+    }
+}
