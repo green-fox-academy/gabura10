fac3eaba7bfda0489b581951c7e244e4b375813f
gabura10 <gabura.alex10@gmail.com>
2020-05-22 12:15:46 +0200

    still working on multiplication table

---
+++ Week-01/Day05/src/MultiplicationTable/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package MultiplicationTable;
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        int x;
+        System.out.println("Please provide a number!");
+        x = sc1.nextInt();
+        for ( int y = 1; y<11; ++y){
+            System.out.println();
+        }
+    }
+}
