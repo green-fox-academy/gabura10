e8a5e11076500492b7aca2e45760ca56a6055e6f
gabura10 <gabura.alex10@gmail.com>
2020-05-25 17:05:45 +0200

    finished the matrix task

--- Week-02/Day01/src/Matrix/Matrix.java
+++ Week-02/Day01/src/Matrix/Matrix.java
@@ -2,6 +2,20 @@ package Matrix;
 
 public class Matrix {
     public static void main(String[] args) {
-        
+        int i= 4;
+        int[][] matrix = {{0, 1},{0, 1}};
+        for (int row = 0; row <i ; row++) {
+            for (int col = 0; col <i ; col++) {
+                if (row == col) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+
+                }
+            System.out.println();
+            }
+
+        }
+
     }
-}
