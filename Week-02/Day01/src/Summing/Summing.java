d6d73f7827a04a8ca2cab9d931e1f3ecdb62b23c
gabura10 <gabura.alex10@gmail.com>
2020-05-25 15:05:50 +0200

    finished 2 more for each group

---
+++ Week-02/Day01/src/Summing/Summing.java
@@ -0,0 +1,16 @@
+package Summing;
+
+public class Summing {
+    static int sum (int x){
+        return x * ( x + 1)/2;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(3));
+
+        }
+
+
+
+
+    }
+
