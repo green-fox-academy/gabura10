60a8a410ede34c4ac4d8eaca3023d6b0b402e18c
gabura10 <gabura.alex10@gmail.com>
2020-05-25 17:22:52 +0200

    fixed matrix to dynamic

--- Week-02/Day01/src/Matrix/Matrix.java
+++ Week-02/Day01/src/Matrix/Matrix.java
@@ -2,10 +2,11 @@ package Matrix;
 
 public class Matrix {
     public static void main(String[] args) {
-        int i= 4;
-        int[][] matrix = {{0, 1},{0, 1}};
-        for (int row = 0; row <i ; row++) {
-            for (int col = 0; col <i ; col++) {
+        int a = 4;
+        int b = 4;
+        int[][] matrix = new int[a][b];
+        for (int row = 0; row <matrix.length ; row++) {
+            for (int col = 0; col < matrix.length; col++) {
                 if (row == col) {
                     System.out.print(1 + " ");
                 } else {
