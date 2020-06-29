2d4f7decc7a6396cd1a464f5bba5beb8dd3ea63e
gabura10 <gabura.alex10@gmail.com>
2020-06-29 14:07:43 +0200

    finished 5th task

---
+++ Week-05/Day01/src/Exercise5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+        int result = numbers.stream()
+                .filter(l-> l%2 != 0)
+                .mapToInt(l -> l)
+                .sum();
+        System.out.println(result);
+    }
+}
